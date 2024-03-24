91d713724e526739af700736d6a0a83a412e72f6
entrezunfredici <superfred2468@gmail.com>
2024-03-24 15:58:36 +0100

    [ADD] init backend

---
+++ openBlogBack/dependencies-install-script.py
@@ -0,0 +1,20 @@
+import os
+print("========================================================================")
+print("bienvennue dans le script d'installation de dépendances")
+commands= {
+    "npm install express",
+    "npm install nodemon --save-dev",
+    "npm install sequelize",
+    "npm install mysql2",
+    "npm install sqlite3",
+    "npm install --save-dev jest supertest",
+    "npm install bcrypt",
+    "npm install http-errors",
+    "npm install jsonwebtoken",
+    "npm install express-openapi-validator",
+    "npm install --save node-cron"
+}
+for command in commands:
+    print("execution de la commande ⇒  "+command)
+    os.system(command)
+print("========================================================================")
