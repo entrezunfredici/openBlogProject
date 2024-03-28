76e603a6a8071c285a5df1a4c80133e6a5d848a4
entrezunfredici <superfred2468@gmail.com>
2024-03-28 16:15:21 +0100

    [IMP] specifications update

--- openBlogFront/src/app/app.module.ts
+++ openBlogFront/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { PostsComponent } from './posts/posts.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { CommentsListComponent } from './comments-list/comments-list.component';
+import { CommentsDetailComponent } from './comments-detail/comments-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    PostsComponent,
+    PostListComponent,
+    CommentsListComponent,
+    CommentsDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ openBlogFront/src/app/comments-detail/comments-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comments-detail',
+  templateUrl: './comments-detail.component.html',
+  styleUrl: './comments-detail.component.scss'
+})
+export class CommentsDetailComponent {
+
+}
