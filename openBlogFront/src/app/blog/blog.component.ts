73ae9a9b5d25717cf812f41942c52534ecb9954b
entrezunfredici <superfred2468@gmail.com>
2024-04-27 17:51:30 +0200

    [IMP] rooting

--- openBlogFront/src/app/blog/blog.component.ts
+++ openBlogFront/src/app/blog/blog.component.ts
@@ -5,6 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.scss'
 })
-export class BlogComponent {
 
+export class BlogComponent {
+  // constructor() { }
+
+  // ngOnInit(): void {
+  //   this.toggleSidebarVisibility();
+  // }
+
+  // toggleSidebarVisibility(): void {
+  //   let sidebar = this.elementRef.nativeElement.querySelector('#sidebar');
+  //   if (sidebar) {
+  //     sidebar.classList.remove("is_invisible");
+  //     sidebar.classList.add("is_visible");
+  //   }
+  // }
 }
