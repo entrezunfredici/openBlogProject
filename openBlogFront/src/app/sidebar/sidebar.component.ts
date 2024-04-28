1ed407031987f7bb4fbdb6e84159deb1716b1b63
entrezunfredici <superfred2468@gmail.com>
2024-04-28 09:19:34 +0200

    little update

--- openBlogFront/src/app/sidebar/sidebar.component.ts
+++ openBlogFront/src/app/sidebar/sidebar.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrls: ['./sidebar.component.scss'] // Correction ici
 })
 export class SidebarComponent {
-  
+  constructor(private router: Router) { }
+
+  navigateToNewPosts() {
+    alert("test");
+    this.router.navigate(['/new']);
+  }
 }
