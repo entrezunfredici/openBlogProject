99bf7e210cb19fc287a7c40481660a027dfd80e1
entrezunfredici <superfred2468@gmail.com>
2024-05-01 17:17:45 +0200

    "[IMP] little update"

--- openBlogBack/controllers/usersController.js
+++ openBlogBack/controllers/usersController.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const usersService = require('../services/db/users')
+const usersService = require('../services/users')
 const createError = require('http-errors');
 const { ServerError } = require('../errors');
 
