70c3a87c292dc8e68236971eed27b3108a0e902d
entrezunfredici <superfred2468@gmail.com>
2024-05-11 18:24:04 +0200

    [ADD] link, and scripts

--- openBlogFront/src/app/services/posts.service.ts
+++ openBlogFront/src/app/services/posts.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Posts } from '../Posts';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
 
-  constructor() { }
+  url ='http://localhost:8000'
+
+  constructor(private http: HttpClient) { }
+
+  getPosts(): Observable<Posts[]> {
+    return this.http.get<Posts[]>(
+    `${this.url}/Posts/all`
+    );
+  }    
 }
