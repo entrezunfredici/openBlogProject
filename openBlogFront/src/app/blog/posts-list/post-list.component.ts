6ef150316a3c31b62f9f2a1c2f1a8c84b127dfa9
entrezunfredici <superfred2468@gmail.com>
2024-05-11 19:26:09 +0200

    [IMP] postList

--- openBlogFront/src/app/blog/posts-list/post-list.component.ts
+++ openBlogFront/src/app/blog/posts-list/post-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-post-list',
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrl: './post-list.component.scss'
 })
 export class PostListComponent {
+  posts : post[] = [
+    {
+      title:"title",
+      creatorname: "kévin",
+      creatoravatar: "",
+      content:"text",
+      nbLike:0,
+      nbDislike:0,
+      nbComment:0,
+      nbReports:0
+    },
+    {
+      title:"title2",
+      creatorname: "kévin",
+      creatoravatar: "",
+      content:"text2",
+      nbLike:0,
+      nbDislike:0,
+      nbComment:0,
+      nbReports:0
+    }
+  ]
+}
 
+export interface post {
+  title: string;
+  creatorname: string;
+  creatoravatar: string;
+  content: string;
+  nbLike:number;
+  nbDislike:number;
+  nbComment:number;
+  nbReports:number
 }
--- openBlogFront/src/app/blog/posts/posts.component.ts
+++ openBlogFront/src/app/blog/posts/posts.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-posts',
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrl: './posts.component.scss'
 })
 export class PostsComponent {
+  @Input() message: post = {
+    title: "",
+    creatorname: "",
+    creatoravatar: "",
+    content: "",
+    nbLike: 0,
+    nbDislike: 0,
+    nbComment: 0,
+    nbReports: 0
+  };
 
+}
+
+export interface post {
+  title: string;
+  creatorname: string;
+  creatoravatar: string;
+  content: string;
+  nbLike:number;
+  nbDislike:number;
+  nbComment:number;
+  nbReports:number
+}
+
+export interface comments {
+  
+}
+
+export interface subjects {
+  
 }
