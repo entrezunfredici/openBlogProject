e3ce965409d4910f382445b6f08652e1259817a5
entrezunfredici <superfred2468@gmail.com>
2024-05-18 10:30:19 +0200

    [ADD] api connection

---
+++ openBlogFront/src/app/model/users.model.ts
@@ -0,0 +1,12 @@
+// posts.model.ts
+export interface Users {
+    id: number;
+    username: string;
+    userPhoto: string;
+    email: string;
+    password: string;
+    nbPosts: number;
+    followers: number;
+    description: number;
+    role: string;
+}
