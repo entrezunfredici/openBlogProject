d091fc15e510c5ec0814661b0f840b63ca4c4700
entrezunfredici <superfred2468@gmail.com>
2024-06-17 19:51:16 +0200

    [ADD] components

--- openBlogFront/src/app/administration/administration.module.ts
+++ openBlogFront/src/app/administration/administration.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AdministrationComponent } from './administration.component';
+import { ReportsListComponent } from './reports-list/reports-list.component';
+import { UsersListComponent } from './users-list/users-list.component';
+import { ReportsComponent } from './reports/reports.component';
+import { UsersComponent } from './users/users.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AdministrationComponent,
+    ReportsListComponent,
+    UsersListComponent,
+    ReportsComponent,
+    UsersComponent
+  ],
   imports: [
     CommonModule
   ]
---
+++ openBlogFront/src/app/administration/reports-list/reports-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reports-list',
+  templateUrl: './reports-list.component.html',
+  styleUrl: './reports-list.component.scss'
+})
+export class ReportsListComponent {
+
+}
